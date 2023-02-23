45d645730a01e0dbaa7ca1e2c8ead2b71f545141
John van de Wetering <john@vdwetering.name>
2023-02-23 17:50:00 +0000

    Updated Graph() function

--- dizx/graph/graph.py
+++ dizx/graph/graph.py
@@ -19,48 +19,34 @@ from typing import Optional
 from .base import BaseGraph
 from .graph_s import GraphS
 
-try:
-	import quizx # type: ignore
-except ImportError:
-	quizx = None
 
-backends = { 'simple': True, 'quizx-vec': False if quizx is None else True }
+backends = { 'simple': True}
 
-def Graph(backend:Optional[str]=None) -> BaseGraph:
-	"""Returns an instance of an implementation of :class:`~pyzx.graph.base.BaseGraph`. 
-	By default :class:`~pyzx.graph.graph_s.GraphS` is used. 
-	Currently ``backend`` is allowed to be `simple` (for the default),
-	or 'graph_tool' and 'igraph'.
-	This method is the preferred way to instantiate a ZX-diagram in PyZX.
+def Graph(dim):
+	return GraphS(dim)
 
-	Example:
-		To construct an empty ZX-diagram, just write::
+# def Graph(backend:Optional[str]=None) -> BaseGraph:
+# 	"""Returns an instance of an implementation of :class:`~pyzx.graph.base.BaseGraph`. 
+# 	By default :class:`~pyzx.graph.graph_s.GraphS` is used. 
+# 	Currently ``backend`` is allowed to be `simple` (for the default),
+# 	or 'graph_tool' and 'igraph'.
+# 	This method is the preferred way to instantiate a ZX-diagram in PyZX.
 
-			g = zx.Graph()
+# 	Example:
+# 		To construct an empty ZX-diagram, just write::
+
+# 			g = zx.Graph()
 		
-	"""
-	if backend is None: backend = 'simple'
-	if backend not in backends:
-		raise KeyError("Unavailable backend '{}'".format(backend))
-	if backend == 'simple': return GraphS()
-	if backend == 'graph_tool': 
-		return GraphGT()
-	if backend == 'igraph': return GraphIG()
-	if backend == 'quizx-vec': return quizx.VecGraph() # type: ignore
-	return GraphS()
+# 	"""
+# 	if backend is None: backend = 'simple'
+# 	if backend not in backends:
+# 		raise KeyError("Unavailable backend '{}'".format(backend))
+# 	if backend == 'simple': return GraphS()
+# 	if backend == 'graph_tool': 
+# 		return GraphGT()
+# 	if backend == 'igraph': return GraphIG()
+# 	if backend == 'quizx-vec': return quizx.VecGraph() # type: ignore
+# 	return GraphS()
 
-Graph.from_json = GraphS.from_json # type: ignore
-Graph.from_tikz = GraphS.from_tikz # type: ignore
-
-try:
-	import graph_tool.all as gt
-	from .graph_gt import GraphGT
-	backends['graph_tool'] = gt
-except ImportError:
-	pass
-try:
-	import igraph as ig
-	from .graph_ig import GraphIG
-	backends['igraph'] = ig 
-except ImportError:
-	pass
+# Graph.from_json = GraphS.from_json # type: ignore
+# Graph.from_tikz = GraphS.from_tikz # type: ignore
